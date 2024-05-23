2e8c5641a7b7a3ac7c3748b777068ba2aa7e1a4b
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2024-05-23 21:22:58 +0000

    Update examples

--- utils/test.py
+++
@@ -1,9 +0,0 @@
-from utils.encode_image import encode_image
-
-def process_receipt(image_path):
-    # Encode the image
-    base64_image = encode_image(image_path)
-    
-    # Console log the image path
-    print(f"Processing: {image_path}")
-    return
